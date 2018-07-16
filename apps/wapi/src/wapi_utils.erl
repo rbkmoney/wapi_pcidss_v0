12f0afc7d0d1e4a6a4007d36ec77445177debe55
Andrey Mayorov <a.mayorov@rbkmoney.com>
2018-07-16 15:41:57 +0300

    Drop meaningless debug printout

--- apps/wapi/src/wapi_utils.erl
+++ apps/wapi/src/wapi_utils.erl
@@ -26,12 +26,7 @@
 
 -spec base64url_to_map(binary()) -> map() | no_return().
 base64url_to_map(Base64) when is_binary(Base64) ->
-    try jsx:decode(base64url:decode(Base64), [return_maps])
-    catch
-        Class:Reason ->
-            _ = lager:debug("decoding base64 ~p to map failed with ~p:~p", [Base64, Class, Reason]),
-            erlang:error(badarg)
-    end.
+    jsx:decode(base64url:decode(Base64), [return_maps]).
 
 -spec map_to_base64url(map()) -> binary() | no_return().
 map_to_base64url(Map) when is_map(Map) ->
